00004629d232da86746469d918ad61a6e453fa3c
Anthony Mesa <admesa@protonmail.com>
2025-10-21 00:02:19 -0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Stack } from '@mantine/core';
+
+function App() {
+  return (
+    <Stack>
+      test
+    </Stack>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import '@mantine/core/styles.css';
+import { createTheme, MantineProvider } from '@mantine/core';
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <MantineProvider theme={theme}>
+      <App />
+    </MantineProvider>
+  </React.StrictMode>
+);
